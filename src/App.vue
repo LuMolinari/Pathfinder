29c62c8af70f00cfdaf59c2f53d7e00582e96757
Luca Molinari <32558805+LuMolinari@users.noreply.github.com>
2021-04-06 17:24:37 -0700

    added vue router

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
 <template>
   <div id="app">
-    <Banner />
-    <Home/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import Banner from './components/Banner.vue'
-import Home from './components/Home.vue'
-
-
-export default {
-  name: 'App',
-  components: {
-    Banner,
-    Home
-  }
-}
-</script>
-
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Merriweather&family=Pacifico&display=swap');
-
-/* This will be any default styling shared throughought the whole application */
-/*All google fonts imported here are available throughout our application */
 #app {
-  font-family: 'Merriweather', serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
