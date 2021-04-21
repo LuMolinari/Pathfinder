3f1ce2ee761d6b0e8e6435f46e2dbd4c464c6476
Armando Guardado <armando.guardado.ag@gmail.com>
2021-04-21 11:02:59 -0700

    Added NavBar commponent starting template

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+ <div>
+  <b-nav tabs>
+    <b-nav-item active><router-link to="/">Home</router-link></b-nav-item>
+    <b-nav-item><router-link to="/about">About</router-link></b-nav-item>
+    <b-nav-item>Another Link</b-nav-item>
+    <b-nav-item disabled>Disabled</b-nav-item>
+  </b-nav>
+</div>
+</template>
+
+<script>
+export default {
+ 
+};
+</script>
+
+<style scoped>
+
+</style>
