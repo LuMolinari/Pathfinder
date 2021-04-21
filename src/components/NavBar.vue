73d4cd689bd3bb797897ddabfceb4c08b11b9c03
Armando Guardado <armando.guardado.ag@gmail.com>
2021-04-21 12:27:35 -0700

    Updated Navigation template to mirror current nav bar but being responsive

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,44 @@
 <template>
- <div>
-  <b-nav tabs>
-    <b-nav-item active><router-link to="/">Home</router-link></b-nav-item>
-    <b-nav-item><router-link to="/about">About</router-link></b-nav-item>
-    <b-nav-item>Another Link</b-nav-item>
-    <b-nav-item disabled>Disabled</b-nav-item>
-  </b-nav>
+<div id="template">
+  <b-navbar id="nav" toggleable="lg" type="dark" variant="dark" >
+    <b-navbar-brand src='#./assets/mountains.png'>Pathfinder</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-navbar-nav>
+        <b-nav-item href="#"><router-link to="/">Home</router-link></b-nav-item>
+        <b-nav-item href="#"><router-link to="/about">About</router-link></b-nav-item>
+        <b-nav-item href="#"><router-link to="/discover">Discover</router-link></b-nav-item>
+      </b-navbar-nav>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </div>
+
 </template>
 
+
+
 <script>
-export default {
- 
-};
+
 </script>
 
 <style scoped>
+#nav {
+    
+    background-image: url('../assets/mountains.png')!important;
+}
+#nav a.router-link-exact-active {
+  color: #8ccfb9;
+}
+
+/* #template{
+    margin-bottom: 50px;
+} */
+
+
 
 </style>
