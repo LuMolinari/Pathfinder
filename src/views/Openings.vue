3cf4d4d5376dec50e1657eb1adc2fa9d4355b4f3
joshuafoster208 <joshua.foster.208w@gmail.com>
2021-04-24 11:54:55 -0700

    Altered to retrieve availabilites up to a year in the future

--- src/views/Openings.vue
+++ src/views/Openings.vue
@@ -16,7 +16,9 @@
 </template>
 
 <script>
-    const curMonth = new Date.getMonth();
+    var date = new Date;
+    var curMonth = date.getMonth() +1;
+    var curYear = date.getFullYear();
     export default {
         data() {
             return {
@@ -32,7 +34,6 @@
                         dates:[
                            
                         ],
-                        description: "",
                     },
                 ],
                 
@@ -45,101 +46,54 @@
                 }
             },
             mounted() {
-                //fetch dates available in the current month from inputed date
-                fetch(
-                    "https://www.recreation.gov/api/camps/availability/campground/" +
-                    this.$route.params.ID +
-                    "/month?start_date=2021-04-01T00%3A00%3A00.000Z"
-                )
-                .then((res) => res.json())
-                //save json dates into availableDates
-                .then((data) => {
-                    const campIDs = [];
-                    console.log(data);
-                    for(const sites in data.campsites){
-                        campIDs.push(sites);
+                var monthDif, monthVal, yearVal;
+                for(var x = 1; x <= 12; x++){
+
+                    if(curMonth+x <= 12){
+                        monthVal = curMonth+x;
+                        yearVal = curYear;
                     }
-                    for(var x = 0; x<campIDs.length; x++){
-                        for(const dates in data.campsites[campIDs[x]].availabilities){
-                            var object = new Date(parseInt(dates.substring(0,4)), 
-                            parseInt(dates.substring(5,7))-1, parseInt(dates.substring(8,10)));
-                            this.available[0].dates.push(object);
+                    else{
+                        monthVal = curMonth + x - 12;
+                        yearVal = curYear + 1;
+                    }
+
+                    if(monthVal >= 10){
+                        monthDif = '-';
+                    }
+                    else{
+                        monthDif = "-0"
+                    }
+
+                    //fetch dates available in the current month and the following 12 months
+                    fetch(
+                        "https://www.recreation.gov/api/camps/availability/campground/" +
+                        this.$route.params.ID +
+                        "/month?start_date=" + yearVal + monthDif +
+                        (monthVal) + "-01T00%3A00%3A00.000Z"
+                    )
+                    .then((res) => res.json())
+                    //save json dates into availableDates
+                    .then((data) => {
+                        const campIDs = [];
+                        for(const sites in data.campsites){
+                            campIDs.push(sites);
                         }
-                        
-                    }
-
-                    this.forceRerender();
-                })
-
-                //fetch dates available in the next month from inputed date
-                fetch(
-                    "https://www.recreation.gov/api/camps/availability/campground/" +
-                    this.$route.params.ID +
-                    "/month?start_date=2021-05-01T00%3A00%3A00.000Z"
-                )
-                .then((res) => res.json())
-                //save json dates into availableDates
-                .then((data) => {
-                    const campIDs = [];
-                    for(const sites in data.campsites){
-                        campIDs.push(sites)
-                    }
-
-                    this.name=data.campsites[campIDs[0]].loop;
-
-                    for(var x = 0; x<campIDs.length; x++){
-                        for(const dates in data.campsites[campIDs[x]].availabilities){
-                            console.log("info = ", data.campsites[campIDs[x]]);
-                            var object = new Date(parseInt(dates.substring(0,4)), 
-                            parseInt(dates.substring(5,7))-1, parseInt(dates.substring(8,10)));
-                            this.available[0].dates.push(object);
+                        for(var x = 0; x<campIDs.length; x++){
+                            for(const dates in data.campsites[campIDs[x]].availabilities){
+                                var object = new Date(parseInt(dates.substring(0,4)), 
+                                parseInt(dates.substring(5,7))-1, parseInt(dates.substring(8,10)));
+                                this.available[0].dates.push(object);
+                            }
+                            
                         }
-                        
-                    }
 
-
-                    this.forceRerender();
-                })
-
-                //fetch dates available 2 months from current date
-                fetch(
-                    "https://www.recreation.gov/api/camps/availability/campground/" +
-                    this.$route.params.ID +
-                    "/month?start_date=2021-06-01T00%3A00%3A00.000Z"
-                )
-                .then((res) => res.json())
-                //save json dates into availableDates
-                .then((data) => {
-                    console.log("curMonth = ", curMonth);
-                    const campIDs = [];
-                    for(const sites in data.campsites){
-                        campIDs.push(sites)
-                    }
-                    for(var x = 0; x<campIDs.length; x++){
-                        for(const dates in data.campsites[campIDs[x]].availabilities){
-                            var object = new Date(parseInt(dates.substring(0,4)), 
-                            parseInt(dates.substring(5,7))-1, parseInt(dates.substring(8,10)));
-                            this.available[0].dates.push(object);
-                        }
-                        
-                    }
-
-                    this.forceRerender();
-                })
-
+                        this.forceRerender();
+                    })
+                }
+                
             },
-            computed: {
-                attributes() {
-                    return this.available.map(a => ({
-                        key: `avail.${a.id}`,
-                        dot: {
-                        backgroundColor: 'red',
-                        },
-                        dates: a.dates,
-                        customData: a,
-                    }));
-                },
-            }
+            
         };
             
 </script>
